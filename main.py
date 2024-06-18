25e60f8867ed4db56f1a0ad69f8afd3ce6fcfa36
sunny.chung@monash.edu <sunnychungty@gmail.com>
2024-06-19 00:40:02 +1000

    package 1st function

--- main.py
+++ main.py
@@ -83,31 +83,27 @@ def add_id(job, JobID, cursor, cnx):
         cnx.rollback()
 
 
-
-def main():
-        # sql connection and read credential
-    cursor, cnx = sql_connection()
-    credentials_path = r"..\credential\login.txt"
-    # chrome_driver_path = r"C:\Users\Sunny\Downloads\chromedriver-win64\chromedriver.exe"
-    chrome_driver_path = r"C:\Users\schu0091\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+def extract_ID(cursor, cnx, credentials_path, driver):
     myusername, mypassword, url = read_credentials(credentials_path)
 
     # Set up the driver
-    driver = setup_driver(chrome_driver_path)
+    # driver = setup_driver(chrome_driver_path)
 
     username_id = "input28"
     password_id = "input36"
     submit_button_id = "preferences_prompt_submit"
 
     # Log in
-    # login_to_website(driver, url, username_id, password_id, submit_button_id, myusername, mypassword)
-    
-    # General site
-    driver.get("https://careers.pageuppeople.com/513/cw/en/listing")
+    login_to_website(driver, url, username_id, password_id, submit_button_id, myusername, mypassword)
+
     time.sleep(5)
 
     # Click the submit button
-    click_button(driver, submit_button_id)
+    click_button(driver, submit_button_id)  
+
+    job_count = 0
+    job_count_new = 0
+    n = 1
 
     while True:
         # Get page source and soup
@@ -118,27 +114,43 @@ def main():
         # Get job links in one page
         jobs = jobs_container.find_all('a', class_='job-link')
         
+
         for job in jobs:
             jobID_match = re.search(r'\/job\/(\d+)\/', str(job))
             if jobID_match:
                 jobID = jobID_match.group(1)
                 if is_dup(jobID, cursor, cnx):
-                    print(f"jobID {jobID} is dup")
+                    print(f"jobID {jobID} already exist in DB")
                     continue
                 else:
                     add_id(job, jobID, cursor, cnx)
-                    
+                    job_count_new += 1
+            job_count += 1
         # Check for the "More Jobs" button and click it
         more_jobs_button = soup.find('a', class_='more-link button')
         if more_jobs_button:
-            print('next page found')
+            print(f'Processing information on page {n + 1}')
             try:
                 driver.get('https://careers.pageuppeople.com/' + soup.find('a', class_='more-link button')['href'])
                 time.sleep(5)
             except Exception as e:
                 print(f"An error occurred: {e}")
         else:
+            print(f"All job openings processed, {job_count} found, {job_count_new} are new")
             break
+
+def main():
+        # sql connection and read credential
+    cursor, cnx = sql_connection()
+    chrome_driver_path = r"C:\Users\Sunny\Downloads\chromedriver-win64\chromedriver.exe"
+    credentials_path = r"..\credential\login.txt"
+
+    # chrome_driver_path = r"C:\Users\schu0091\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+    driver = setup_driver(chrome_driver_path)
+
+    extract_ID(cursor, cnx, credentials_path, driver)
+
+
              
     while(True):
         pass
