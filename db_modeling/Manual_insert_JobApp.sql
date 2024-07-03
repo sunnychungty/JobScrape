d79c857786ace58f847f68d5aad480700d620e21
sunny.chung@monash.edu <sunnychungty@gmail.com>
2024-07-04 01:14:01 +1000

    job app

--- db_modeling/Manual_insert_JobApp.sql
+++ db_modeling/Manual_insert_JobApp.sql
@@ -3,13 +3,15 @@ SET @nextJobAppID = (SELECT IFNULL(MAX(JobAppID), 0) + 1 FROM Job_app);
 INSERT INTO Job_app (JobAppID, app_status, app_date, JobID, company)
 VALUES (@nextJobAppID, 
 		'Applied', 
-		"2024-07-02", 
-		'1526', 
-		(Select company_name from JobsOpening_temp where JobID = "1526"));
+		"2024-07-04", 
+		'2163', 
+		(Select company_name from JobsOpening_temp where JobID = "2163"));
 
 
 UPDATE Job_app
-SET company = "RACV"
-WHERE JobAppID = 3;
+SET latest_status_update = current_date()
+WHERE JobAppID = 6;
+
+select * from JobsOpening_temp order by JobID desc limit 1;
 
 select * from Job_app;
